5ec2968468db8106764526fd7960922491c11418
WP-Jorge <853539461@qq.com>
2021-06-17 12:19:40 +0800

    修复了footer在小分辨率设备下未撑满屏幕的问题

--- src/App.vue
+++ src/App.vue
@@ -10,46 +10,46 @@
 </template>
 
 <script>
-	import Header from 'components/content/header/Header.vue'
-	import Navbar from 'components/content/navbar/Navbar.vue'
-	import Footer from 'components/content/footer/Footer.vue'
+import Header from 'components/content/header/Header.vue'
+import Navbar from 'components/content/navbar/Navbar.vue'
+import Footer from 'components/content/footer/Footer.vue'
 
-	export default {
-		components: {
-			Header,
-			Navbar,
-			Footer
-		},
-		data() {
-			return {
-				isRouterAlive: true
-			}
-		},
-		provide() {
-			return {
-				reload: this.reload
-			}
-		},
-		methods: {
-			reload() {
-				this.isRouterAlive = false
-				this.$nextTick(() => {
-					this.isRouterAlive = true
-				})
-			}
+export default {
+	components: {
+		Header,
+		Navbar,
+		Footer
+	},
+	data() {
+		return {
+			isRouterAlive: true
+		}
+	},
+	provide() {
+		return {
+			reload: this.reload
+		}
+	},
+	methods: {
+		reload() {
+			this.isRouterAlive = false
+			this.$nextTick(() => {
+				this.isRouterAlive = true
+			})
 		}
 	}
+}
 </script>
 
 <style lang="scss">
-	@import 'assets/sass/base.scss';
+@import 'assets/sass/base.scss';
 
-	#app {
-		/*保证footer是相对于container位置绝对定位*/
-		position: relative;
-		min-height: 100%;
-		/*设置padding-bottom值大于等于footer的height值，以保证main的内容能够全部显示出来而不被footer遮盖；*/
-		padding-bottom: 50px;
-		box-sizing: border-box;
-	}
+#app {
+	/*保证footer是相对于container位置绝对定位*/
+	position: relative;
+	min-height: 100%;
+	/*设置padding-bottom值大于等于footer的height值，以保证main的内容能够全部显示出来而不被footer遮盖；*/
+	padding-bottom: 50px;
+	box-sizing: border-box;
+}
 </style>
--- src/components/content/footer/Footer.vue
+++ src/components/content/footer/Footer.vue
@@ -5,31 +5,32 @@
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				rights: 'Z09418233 朱心熹 ©2020'
-			}
+export default {
+	data() {
+		return {
+			rights: 'Z09418233 朱心熹 ©2020'
 		}
 	}
+}
 </script>
 
 <style lang="scss" scoped>
-	@import 'assets/sass/base.scss';
+@import 'assets/sass/base.scss';
 
-	.footer {
-		position: absolute;
-		width: 100%;
-		height: 50px;
-		bottom: 0px;
-		left: 0px;
-		right: 0;
-		text-align: center;
-		background: #333;
+.footer {
+	position: absolute;
+	width: 100%;
+	min-width: 1200px;
+	height: 50px;
+	bottom: 0px;
+	left: 0px;
+	right: 0;
+	text-align: center;
+	background: #333;
 
-		font {
-			line-height: 50px;
-			color: $gray_color;
-		}
+	font {
+		line-height: 50px;
+		color: $gray_color;
 	}
+}
 </style>
